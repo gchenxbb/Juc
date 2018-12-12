44468a831ebdbe6e5bd9f88c8282777127520ca6
Nico <chenguang0508@163.com>
2018-12-12 21:24:07 +0800

    增加解释

--- app/src/main/java/com/gc/multi/LockInterruptiblyRunnable.java
+++ app/src/main/java/com/gc/multi/LockInterruptiblyRunnable.java
@@ -21,7 +21,6 @@ public class LockInterruptiblyRunnable implements Runnable {
         }
     }
 
-
     public void invokeM() throws InterruptedException {
         lock.lockInterruptibly();
         try {
