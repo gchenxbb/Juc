09e3790cd6f858b5898f439b31b6ca685cb597b3
chenguang <chenguang0508@163.com>
2020-01-05 23:39:18 +0800

    Summary：整理提交

--- app/src/main/java/com/gc/multi/WaitNotifyRunnable.java
+++ app/src/main/java/com/gc/multi/WaitNotifyRunnable.java
@@ -11,12 +11,12 @@ public class WaitNotifyRunnable implements Runnable {
     public void secondMethod() throws Exception {
         String threadName = Thread.currentThread().getName();
         Thread.sleep(2000);//
-        Log.d(TAG, threadName + "线程：secondMethod()方法");
         synchronized (this) {
             Log.d(TAG, threadName + "线程，竞争获得锁，进入休眠" );
             Thread.sleep(10000);//不会释放锁
-            Log.d(TAG, threadName + "线程，通知唤醒wait线程" );
+            Log.d(TAG, threadName + "线程，sleep结束，通知唤醒wait线程" );
             notifyAll();
+            Log.d(TAG, threadName + "线程，即将再次sleep，不会释放锁，被唤醒的wait，进入锁等待池" );
             Thread.sleep(5000);//不会释放锁
         }
     }
@@ -24,7 +24,6 @@ public class WaitNotifyRunnable implements Runnable {
     public void run() {
         try {
             String threadName = Thread.currentThread().getName();
-            Log.d(TAG, threadName + "线程：firstMethod()方法");
             synchronized (this) {
                 Log.d(TAG, threadName + "线程，竞争获得锁，进入休眠" );
                 this.wait();//释放锁。
